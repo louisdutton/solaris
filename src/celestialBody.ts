d6364700174f82714b34f031b2ceee822441d9e4
Louis Dutton <clouisdutton@gmail.com>
2022-01-25 22:37:26 +0000

    added orbital indicator ui

--- src/celestialBody.ts
+++ src/celestialBody.ts
@@ -2,64 +2,84 @@ import * as THREE from "three";
 import * as MAT from "./materials";
 import { Random } from "./global";
 
-export default class CelestialBody {
-	mesh: THREE.Mesh | THREE.LOD;
-	positionalAudio: THREE.PositionalAudio;
-	oscillalator: OscillatorNode;
-	orbit?: THREE.Spherical;
-	angularVelocity: THREE.Euler;
+export const createCelestialBody = (
+	listener: THREE.AudioListener,
+	frequency: number
+) => {
+	const color = 0xffffff * Random.value();
+	const scale = 0.5 * Random.value();
+	const details = [8, 2, 1];
 
-	constructor(listener: THREE.AudioListener, frequency: number) {
-		const color = 0xffffff * Random.value();
-		const scale = 0.5 * Random.value();
-		const details = [8, 2, 1];
+	const texture = new THREE.TextureLoader().load("/textures/moonbump1k.jpg");
+	const options = [
+		{ bumpMap: texture, bumpScale: 0.008, color: color },
+		{ color: color },
+		{ color: color },
+	];
 
-		const texture = new THREE.TextureLoader().load("/textures/moonbump1k.jpg");
-		const options = [
-			{ bumpMap: texture, bumpScale: 0.008, color: color },
-			{ color: color },
-			{ color: color },
-		];
+	// LODs (level of detail)
+	const lod = new THREE.LOD();
+	for (let i = 0; i < 3; i++) {
+		const geometry = new THREE.IcosahedronBufferGeometry(scale, details[i]);
+		const material = new THREE.MeshStandardMaterial(options[i]);
+		lod.addLevel(new THREE.Mesh(geometry, material), 25 * i);
+	}
 
-		// LODs (level of detail)
-		const lod = new THREE.LOD();
-		for (let i = 0; i < 3; i++) {
-			const geometry = new THREE.IcosahedronBufferGeometry(scale, details[i]);
-			const material = new THREE.MeshStandardMaterial(options[i]);
-			lod.addLevel(new THREE.Mesh(geometry, material), 25 * i);
-		}
+	// atmosphere
+	const material = MAT.atmosphere.clone();
+	material.uniforms.color.value = new THREE.Color(color);
 
-		// atmosphere
-		const material = MAT.atmosphere.clone();
-		material.uniforms.color.value = new THREE.Color(color);
+	lod.add(
+		new THREE.Mesh(
+			new THREE.IcosahedronBufferGeometry(scale * 1.1, 4),
+			material
+		)
+	);
 
-		lod.add(
-			new THREE.Mesh(
-				new THREE.IcosahedronBufferGeometry(scale * 1.05, 4),
-				material
+	// Oscillator
+	const osc = listener.context.createOscillator(); // default sine wave
+	osc.type = "sine";
+	osc.frequency.value = frequency; // harmonic series
+	osc.start();
+
+	// Positional audio source (three.js) attenuation model: 1/x
+	const sound = new THREE.PositionalAudio(listener);
+	sound.setRolloffFactor(0.4);
+	// TODO: maybe create a PR to allow multiple node types
+	//@ts-ignore
+	sound.setNodeSource(osc);
+	sound.panner.panningModel = "equalpower";
+
+	lod.add(sound);
+	return lod;
+};
+
+export function createOrbit(radius: number) {
+	var points: THREE.Vector3[] = [];
+
+	// 360 full circle will be drawn clockwise
+	for (let i = 0; i <= 360; i++) {
+		points.push(
+			new THREE.Vector3(
+				Math.sin(i * (Math.PI / 180)) * radius,
+				Math.cos(i * (Math.PI / 180)) * radius,
+				0
 			)
 		);
-
-		// Oscillator
-		const osc = listener.context.createOscillator(); // default sine wave
-		osc.type = "sine";
-		osc.frequency.value = frequency; // harmonic series
-		osc.start();
-
-		// Positional audio source (three.js) attenuation model: 1/x
-		const sound = new THREE.PositionalAudio(listener);
-		sound.setRolloffFactor(0.4);
-		// TODO: maybe create a PR to allow multiple node types
-		//@ts-ignore
-		sound.setNodeSource(osc);
-		sound.panner.panningModel = "equalpower";
-
-		this.positionalAudio = sound;
-		this.oscillalator = osc;
-
-		this.mesh = lod;
-		this.mesh.add(sound);
-
-		this.angularVelocity = new THREE.Euler(0.002);
 	}
+
+	const geometry = new THREE.BufferGeometry().setFromPoints(points);
+
+	const material = new THREE.LineDashedMaterial({
+		color: 0xffffff,
+		linewidth: 1,
+		scale: 1,
+		dashSize: 0.1,
+		gapSize: 0.1,
+	});
+
+	const line = new THREE.Line(geometry, material);
+	line.computeLineDistances();
+
+	return line;
 }
--- src/solaris.ts
+++ src/solaris.ts
@@ -5,7 +5,7 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer.js";
 import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass.js";
 import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass";
-import CelestialBody from "./celestialBody";
+import { createOrbit, createCelestialBody } from "./celestialBody";
 import Star from "./star";
 import * as MAT from "./materials";
 import { Random } from "./global";
@@ -71,31 +71,34 @@ const sun = new Star(listener, F0);
 scene.add(sun.mesh);
 
 // Celestial bodies
-const celestialBodies: CelestialBody[] = [];
+const orbits: THREE.Object3D[] = [];
 for (let i = 1; i < celestialCount; i++) {
-	const body = (celestialBodies[i] = new CelestialBody(listener, F0 + F0 * i));
-	initOrbit(body, i);
-	scene.add(body.mesh);
+	const radius = 1 + i;
+	const orbit = createOrbit(radius);
+	orbit.rotation.z = Random.range(0, Math.PI * 2);
+
+	const body = createCelestialBody(listener, F0 + F0 * i);
+	orbit.add(body);
+	body.position.x = radius;
+	orbits.push(orbit);
+	scene.add(orbit);
 }
 
-// Controls
+// Camera Controls
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = 0.05;
 controls.minDistance = 5;
 controls.maxDistance = 15;
 controls.enablePan = false;
+camera.position.z = 15;
 
 // Update loop
 let time = 0;
 const update = () => {
 	requestAnimationFrame(update);
 
-	// sun.update()
-	celestialBodies.forEach((body) => {
-		rotate(body);
-		if (body.orbit) orbit(body);
-	});
+	orbits.forEach((o) => (o.rotation.z += 0.001));
 
 	time += 0.001;
 	MAT.star.uniforms.time.value = time;
@@ -130,20 +133,20 @@ window.onfocus = () => {
 	ctx.resume();
 };
 
-function initOrbit(body: CelestialBody, index: number) {
-	const phi = Random.value() * Math.PI * 2;
-	body.orbit = new THREE.Spherical(1 + index, phi, 0);
-	body.mesh.position.x = body.orbit.radius;
-}
+// function initOrbit(body: CelestialBody, index: number) {
+// 	const phi = Random.value() * Math.PI * 2;
+// 	body.orbit = new THREE.Spherical(1 + index, phi, 0);
+// 	body.mesh.position.x = body.orbit.radius;
+// }
 
-function orbit(body: CelestialBody) {
-	if (!body.orbit) return;
-	body.orbit.phi += 0.001;
-	body.mesh.position.setFromSpherical(body.orbit);
-}
+// function orbit(body: CelestialBody) {
+// 	if (!body.orbit) return;
+// 	body.orbit.phi += 0.001;
+// 	body.mesh.position.setFromSpherical(body.orbit);
+// }
 
-function rotate(body: CelestialBody) {
-	body.mesh.rotation.x += body.angularVelocity.x;
-	body.mesh.rotation.y += body.angularVelocity.y;
-	body.mesh.rotation.z += body.angularVelocity.z;
-}
+// function rotate(body: CelestialBody) {
+// 	body.mesh.rotation.x += body.angularVelocity.x;
+// 	body.mesh.rotation.y += body.angularVelocity.y;
+// 	body.mesh.rotation.z += body.angularVelocity.z;
+// }
